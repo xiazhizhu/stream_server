53c0dfe7f265d1ba1c1cfbe593e0eea3aff96c02
xiazhizhu <zhizhu1989@126.com>
2018-08-10 10:57:27 +0800

    delete some useless codes

--- webServer/server.py
+++ webServer/server.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
-#!-*-coding:utf-8-*-
-from flask import Flask
-from flask import request
-from flask import jsonify
+#-*-coding:utf-8-*-
+
+import logging, json, time, threading
+from flask import Flask, request, jsonify
 from gevent import monkey
-import logging
-import json
-import time
-import threading
-from dealDb import getAddress, gettimeoutstreamlist, setAllowStreamlist
+from dealDb import getAddress, setAllowStreamlist
 from timer_event import get_timeout_list
 
 
@@ -19,96 +15,41 @@ HOST="0.0.0.0"
 #设定访问端口
 PORT="9999"
 #设置调试模式(on/off)
-DEBUG=False
+# DEBUG=False
 #TU唯一识别码
 VINCODE="123456789"
 #时间戳
 TIMESTAMP=0
 
 
-#从超时接口获取的list数据,进行封装处理
-# def list_2_dict_package(result):
-	# new_list = []
-	# if len(result) != 0:
-		# for index in range(len(result)):
-			# new_dict = {}
-			# new_dict["vinCode"] = result[index]
-			# new_list.append(new_dict)
-	# server.logger.debug('new_list:%s', new_list)
-	# return new_list
-
-
-
-
-
 server = Flask(__name__)
 
-# @server.route('/', methods=['GET', 'POST'])
-# def index():
-	# server.logger.debug('index')
-	# return jsonify(
-	# index="请输入正确的访问URL"
-	# )
-
-# @server.route('/v1/darwin/gettimeoutstreamlist/', methods=['GET'])
-# def get_method_info():
-	# server.logger.debug('request.host:%s', request.host)
-	# server.logger.debug('request.url:%s', request.url)
-	# 调用获取超时接口
-	# try:
-		# result = gettimeoutstreamlist()
-	# except:
-		# server.logger.debug('gettimeoutstreamlist callback error')
-		# return jsonify(
-		# error='gettimeoutstreamlist callback error')
-	# else:
-		# server.logger.debug('result:%s', result)
-		# list_num = len(result)
-		# if list_num == 0:
-			# result_list=[]
-		# else:
-			# result_list = list_2_dict_package(result)
-		# return jsonify(
-		# status=0,
-		# number=list_num,
-		# list=result_list
-		# )
 
 @server.route('/v1/darwin/getaddress/', methods=['POST'])
 def post_method_info():
 	server.logger.debug('request.headers:%s', request.headers)
 	server.logger.debug('request.host:%s', request.host)
 	server.logger.debug('request.url:%s', request.url)
-	# print 'request.url:%s', request.url
 	#获取原始post数据
 	data = request.get_data()
 	server.logger.debug('request.data:%s', data)
-	# print 'request.data:%s', data
 	#转成Python数据结构
 	data_str = json.loads(data)
 	server.logger.debug('request.data[vinCode]:%s', data_str['vinCode'])
-	# print 'request.data[vinCode]:%s', data_str['vinCode']
 	VINCODE = data_str['vinCode']
 	server.logger.debug('VINCODE:%s', VINCODE)
-	# print 'VINCODE:%s', VINCODE
 	server.logger.debug('request.data[liveType]:%s', data_str['liveType'])
-	# print 'request.data[liveType]:%s', data_str['liveType']
-	
 	TIMESTAMP=int(time.time())
 	server.logger.debug('TIMESTAMP:%s', TIMESTAMP)
-	# print 'TIMESTAMP:%s', TIMESTAMP
+
 	
 	#调用检测接口
 	try:
 		ret = getAddress(VINCODE)
 	except:
 		server.logger.debug('getAddress callback error')
-		# print 'getAddress callback error'
-		# return jsonify(
-		# error='getAddress callback error')
 	else:
 		server.logger.debug('ret:%s', ret)
-		# print 'ret:%s', ret
 		if ret[0]==0:
 			video_stream=str(VINCODE)+str(TIMESTAMP)+".sdp"
 		elif ret[0]==1:
@@ -121,7 +62,6 @@ def post_method_info():
 				setAllowStreamlist(video_stream)
 			except:
 				server.logger.debug('setAllowStreamlist callback error')
-				# print 'setAllowStreamlist callback error'
 		return jsonify(
 		status=get_status,
 		exist=ret[0],
@@ -132,9 +72,8 @@ def post_method_info():
 		)
 
 def web_server():
-	#server.debug=DEBUG	#debug模式
 	logging.basicConfig(level=logging.DEBUG)
-	handler = logging.FileHandler('server.log', encoding='UTF-8')
+	handler = logging.FileHandler('webServer.log', encoding='UTF-8')
 	handler.setLevel(logging.DEBUG)
 	logging_format = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s')
 	handler.setFormatter(logging_format)
--- webServer/timer_event.py
+++ webServer/timer_event.py
@@ -1,48 +1,46 @@
 #!/usr/bin/env python
-#!-*-coding:utf-8-*-
+#-*-coding:utf-8-*-
 
-import threading
-import time
-import logging
-import json,urllib2
-
+import threading, time, logging, json, urllib2
 from dealDb import gettimeoutstreamlist
 
+##TSP callback
 URL = "http://tomcat-microservices.stg02.internal.caritc.de:8080/nev-biz-proxy/services/remoteVideoCallback/closePush/"
+##定时时长(单位:秒)
 TIMESTEP=120
+##日志文件
+logging.basicConfig(level=logging.DEBUG, filename='webServer_timeout.log', filemode='w', format='%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s')  
+
 
 def post_method_2_tps(text):
 	if(len(text) == 0):
-		print "timeout list is empty!!!"
+		logging.debug("timeout list is empty!!!")
 	else:
 		text = json.dumps(text)
-		#print text
-		print "post_method_2_tps: %s", text
+		logging.debug("%s", text)
 		header_dict = {"User-Agent":"Apache-HttpClient/4.1.1 (java 1.5)","Content-Type":"application/json","Accept-Encoding": "gzip,deflate"}
 		req = urllib2.Request(url=URL,data=text,headers=header_dict)
 		result = urllib2.urlopen(req)
 		result = result.read()
-		print "post_method_2_tps: %s", result
+		logging.debug("%s", result)
 
 def get_timeout_list():
-	# logger.debug("get_timeout_list")
-	# logging.basicConfig(level=logging.DEBUG)
-	# handler = logging.FileHandler('server_timeout.log', encoding='UTF-8')
-	# handler.setLevel(logging.DEBUG)
-	# logging_format = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s')
-	# handler.setFormatter(logging_format)
-	# logger.addHandler(handler)
 	#调用获取超时接口
-	result = gettimeoutstreamlist()
+	try:
+		result = gettimeoutstreamlist()
+	except:
+		logging.debug("gettimeoutstreamlist callback error")
 	list_num = len(result)
-	print "get_timeout_list: %s", list_num
+	logging.debug("%s", list_num)
 	if list_num == 0:
 		result_list=[]
 	else:
 		result_list=result
-	print result_list
-	post_method_2_tps(result_list)
-	#//处理TSP相关部分
-	#//将结果发送给TSP
-	t = threading.Timer(TIMESTEP, get_timeout_list).start()
+	logging.debug(result_list)
+	#调用TSP callback接口
+	try:
+		post_method_2_tps(result_list)
+	except:
+		logging.debug("post_method_2_tps callback error")
+	threading.Timer(TIMESTEP, get_timeout_list).start()
 
